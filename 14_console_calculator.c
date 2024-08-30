6c13a41ec61b3b4fcb65ec45b05ee6a4347b8172
Amrit Kumar Yadav <kittyamrit005@gmail.com>
2024-08-30 09:38:51 +0530

    new file:   14_console_calculator.c

---
+++ 14_console_calculator.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+#include <math.h>
+
+int main() {
+    double a,b;
+    int d,e;
+    char c;
+    scanf("%lf %lf %c",&a,&b,&c);
+    
+    d = (int)a;
+    e = (int)b;
+    
+    switch(c){
+    case '+':
+        printf("%.5f",(a+b));
+        break;
+    case '-':
+        printf("%.5f",(a-b));
+        break;
+    case '*':
+        printf("%.5f",(a*b));
+        break;
+    case '/':
+        (b==0)? printf("The operation is invalid") : printf("%.5f",(a/b));
+        break;
+    case '%':
+        printf("%d",(d%e));
+        break;
+    case '^':
+        printf("%.5f",(pow(a,b)));
+        break;
+    case '&':
+        printf("%d",(d&e));
+        break;
+    case '|':
+        printf("%d",(d|e));
+        break;
+    default:
+        printf("The operation is invalid");
+    }
+
+    return 0;
+}
