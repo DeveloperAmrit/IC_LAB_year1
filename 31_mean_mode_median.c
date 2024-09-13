b19516c897104d0bb35da75f94001e99b4d2f761
Amrit Kumar Yadav <kittyamrit005@gmail.com>
2024-09-13 11:35:48 +0530

    modified:   31_mean_mode_median.c

--- 31_mean_mode_median.c
+++ 31_mean_mode_median.c
@@ -18,25 +18,6 @@ int main(){
     
     printf("%.2lf",mean);
 
-    // mode
-    int mode;
-    int max_count = 0;
-    
-    for(int i=0;i<n;i++){
-        int e = arr1[i];
-        int count=0;
-        for(int j=0;j<n;j++){
-            if(e==arr1[j]){
-                count++;
-            }
-        }
-        if(count>max_count){
-            max_count = count;
-            mode = arr1[i];
-        }
-    }
-    printf("\n%d",mode);
-
     //sorting the array
     // sorting code by me (infinite swapping) may take some time
     // we can also use qsort() from <stlib.h> but that had not been taught
@@ -64,7 +45,28 @@ int main(){
         }
         
     }
-    
 
+    // mode
+    int mode = arr1[0];
+    int max_count = 1;
+    
+    for(int i=0;i<n;i++){
+        int e = arr1[i];
+        int count=0;
+        for(int j=0;j<n;j++){
+            if(e==arr1[j]){
+                count++;
+            }
+        }
+        if(count>max_count){
+            max_count = count;
+            mode = arr1[i];
+        }
+    }
+    printf("\n%d",mode);
+
+
+    
+    // median
     (n%2==0)? printf("\n%.2f",(arr1[n/2-1]+arr1[(n/2)])/(2.0)) : printf("\n%d",arr1[(n/2)]);    
 }
