d382e08651c19959938f3c66972c82e245aefb99
Amrit Kumar Yadav <kittyamrit005@gmail.com>
2024-09-13 11:19:31 +0530

    new file:   31_mean_mode_median.c

---
+++ 31_mean_mode_median.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+
+int main(){
+
+    int n;
+    scanf("%d",&n);
+    int arr1[n];
+
+    // mean
+    double sum=0;
+    double mean;
+
+    for(int i=0;i<n;i++){
+        scanf("%d",&arr1[i]);
+        sum += arr1[i];
+    }
+    mean = (sum/n);
+    
+    printf("%.2lf",mean);
+
+    // mode
+    int mode;
+    int max_count = 0;
+    
+    for(int i=0;i<n;i++){
+        int e = arr1[i];
+        int count=0;
+        for(int j=0;j<n;j++){
+            if(e==arr1[j]){
+                count++;
+            }
+        }
+        if(count>max_count){
+            max_count = count;
+            mode = arr1[i];
+        }
+    }
+    printf("\n%d",mode);
+
+    //sorting the array
+    // sorting code by me (infinite swapping) may take some time
+    // we can also use qsort() from <stlib.h> but that had not been taught
+
+    int b = 0;
+    while(1)
+    {
+        for(int i=0;i< ((sizeof arr1/ sizeof arr1[0])-1);i++){
+            if(arr1[i]>arr1[i+1]){
+                arr1[i] = arr1[i] - arr1[i+1];
+                arr1[i+1] = arr1[i+1] + arr1[i];
+                arr1[i] = arr1[i+1] - arr1[i];
+            }
+        }
+        for(int i=0;i<((sizeof arr1/ sizeof arr1[0])-1);i++){
+            if(arr1[i]<=arr1[i+1]){
+                b++;
+            }
+        }
+        if(b==((sizeof arr1/ sizeof arr1[0])-1)){
+            break;
+        }
+        else{
+            b=0;
+        }
+        
+    }
+    
+
+    (n%2==0)? printf("\n%.2f",(arr1[n/2-1]+arr1[(n/2)])/(2.0)) : printf("\n%d",arr1[(n/2)]);    
+}
